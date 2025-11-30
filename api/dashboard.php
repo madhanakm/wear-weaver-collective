39d11ad92c44f4bbcc4fc02bc3c988602d5e238c
madhanakm <49429368+madhanakm@users.noreply.github.com>
2025-11-30 09:41:33 +0530

    favicon fix

--- api/dashboard.php
+++ api/dashboard.php
@@ -16,6 +16,7 @@ $page = $_GET['page'] ?? 'contacts';
 <html>
 <head>
     <title>Admin Dashboard</title>
+    <link rel="icon" type="image/png" href="nsg_logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         body { 
